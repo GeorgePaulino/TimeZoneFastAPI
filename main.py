aef4b3bdb0227c13af434ed5e0d01cdc4e546eac
George P <george7paulino@gmail.com>
2023-05-07 04:58:23 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+import pytz
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    time_gmt_minus_3 = datetime.now(pytz.timezone("Etc/gmt+3"))
+    time_gmt_minus_5 = datetime.now(pytz.timezone("Etc/gmt+5"))
+    return {
+        "time_gmt-3" : time_gmt_minus_3,
+        "time_gmt-5" : time_gmt_minus_5
+    }
+
+@app.get("/time/{zone}")
+def read_time(zone: str):
+    _zone = zone.replace('+', '-') if '+' in zone else zone.replace('-', '+')
+    gmt_zone = pytz.timezone("Etc/" + _zone)
+    current_time = datetime.now(gmt_zone)
+    return {
+        "time" : current_time
+    }
